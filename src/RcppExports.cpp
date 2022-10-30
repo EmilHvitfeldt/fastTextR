4542f33930b03ddc7b0e446bbc242abce2b62c0b
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2022-10-30 16:48:10 -0700

    redocument

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // clean_text
 std::vector<std::string> clean_text(std::vector<std::string> x);
 RcppExport SEXP _fastTextR_clean_text(SEXP xSEXP) {
