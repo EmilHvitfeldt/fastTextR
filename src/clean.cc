01d2d09a87649a7a554ad3d8918cec09ce7520c7
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2022-10-30 17:00:09 -0700

    Use || not | to appease CRAN

--- src/clean.cc
+++ src/clean.cc
@@ -12,11 +12,11 @@ int is_break(std::string s) {
 }
 
 
-// s1 == " " | s1 == "\a" | s1 == "\b" | s1 == "\f" 
+// s1 == " " | s1 == "\a" | s1 == "\b" | s1 == "\f"
 // | s1 == "\n" | s1 == "\r" | s1 == "\t" | s1 == "\v"
 int is_control_char(std::string s1) {
-    if ( (s1 == " ") | (s1 == "\a") | (s1 == "\b") | (s1 == "\f") 
-       | (s1 == "\n") | (s1 == "\r") | (s1 == "\t") | (s1 == "\v") ) {
+    if ( (s1 == " ") || (s1 == "\a") || (s1 == "\b") || (s1 == "\f")
+       || (s1 == "\n") || (s1 == "\r") || (s1 == "\t") || (s1 == "\v") ) {
         return 1;
     }
     return 0;
@@ -26,8 +26,8 @@ int is_control_char(std::string s1) {
 // s1 == "\'" | s1 == "\"" | s1 == "." | s1 == "," | s1 == "("
 //   | s1 == ")" | s1 == "!" | s1 == "\?"
 int is_punctation(std::string s1) {
-    if ( (s1 == "\'") | (s1 == "\"") | (s1 == ".") | (s1 == ",") | (s1 == "(")
-       | (s1 == ")") | (s1 == "!") | (s1 == "\?") ) {
+    if ( (s1 == "\'") || (s1 == "\"") || (s1 == ".") || (s1 == ",")
+       || (s1 == "(") || (s1 == ")") || (s1 == "!") || (s1 == "\?") ) {
         return 1;
     }
     return 0;
@@ -37,7 +37,7 @@ int is_punctation(std::string s1) {
 // ## add space
 // ## "'", '"', ".", ",", "(", ")", "!", "?", ""
 // ## replace with " "
-// ## "<br />", ";", ":", 
+// ## "<br />", ";", ":",
 // ## "<br />"
 // [[Rcpp::export]]
 std::vector<std::string> clean_text(std::vector<std::string> x) {
@@ -59,7 +59,7 @@ std::vector<std::string> clean_text(std::vector<std::string> x) {
                 y[i] += s1;
                 if ( j + 1 <  n ) {
                     if ( s2 != " ") {
-                        y[i] += " ";       
+                        y[i] += " ";
                     }
                 }
             } else if ( is_control_char(s1) ) {
@@ -67,19 +67,19 @@ std::vector<std::string> clean_text(std::vector<std::string> x) {
                 if ( s0 != " " ) {
                     y[i] += " ";
                 }
-            } else if ( (s1 == ";") | (s1 == ":") ) {
+            } else if ( (s1 == ";") || (s1 == ":") ) {
                 if ( s0 != " " ) {
                     y[i] += " ";
                 }
-            } else if ( (s1 == "<") & (s2 == "b") & (n - j > 4) ) {
+            } else if ( (s1 == "<") && (s2 == "b") && (n - j > 4) ) {
                 size_t len = ( 6 < (n - j) ) ? 6 : (n-j);
                 j += is_break(x[i].substr(j, len));
                 if ( j > 0 ) {
                     if ( s0 != " " ) {
-                        y[i] += " ";       
+                        y[i] += " ";
                     }
                 } else {
-                    y[i] += s1;    
+                    y[i] += s1;
                 }
             } else {
                 y[i] += s1;
